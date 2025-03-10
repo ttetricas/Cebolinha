5e459ec6d96470f0b0553a587651847bc5553094
ttetricas <estermeireles89@gmail.com>
2025-03-10 20:41:57 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+        
+        if (!string.IsNullOrEmpty(frase))
+        {
+            string fraseCebolinha = frase.Replace("r", "l").Replace("R", "L");
+            Console.WriteLine("Cebolinha diz: " + fraseCebolinha);
+        }
+        else
+        {
+            Console.WriteLine("Você não digitou nada!");
+        }
